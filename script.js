17dd7cf4a7989d807cd435f12e6d0e71d73cfc85
JulioAngga <103113203+JulioAngga@users.noreply.github.com>
2024-11-26 08:52:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// JavaScript
+function toggleMenu() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('active');
+}
+
+function showPage(page) {
+    const sections = document.querySelectorAll('.content');
+    sections.forEach(section => {
+        section.style.display = 'none';
+    });
+
+    document.getElementById(page).style.display = 'block';
+}
